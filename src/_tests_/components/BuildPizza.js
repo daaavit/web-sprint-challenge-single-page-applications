b2d2e5dcaf176e9300cff44d6f1a2b269027f786
Davit <daavit@yahoo.com>
2021-12-17 15:36:49 -0500

    still working

--- src/_tests_/components/BuildPizza.js
+++ src/_tests_/components/BuildPizza.js
@@ -1,7 +1,18 @@
-import React from "react";
-import './BuildPizza.css'
+import React, { useState } from "react";
+import "./BuildPizza.css";
 
 export default function BuildPizza() {
+  const [pizzaSizePrice, setPizzaSizePrice] = useState(0);
+
+  const onSelectChange = (selectedValue) => {
+    let value = selectedValue.target.value;
+
+    if (value !== 0) {
+      setPizzaSizePrice(value);
+    }
+  };
+
+  console.log("pizzaSizePrice", pizzaSizePrice); 
   return (
     <div className="buildPizza">
       <h4>Build Your Own Pizza</h4>
@@ -10,11 +21,12 @@ export default function BuildPizza() {
         Required
       </div>
       <div className="dropDownSelect">
-          <select>
-              <option>--Large--</option>
-              <option>--Medium--</option>
-              <option>--Small--</option>
-          </select>
+        <select onChange={onSelectChange}>
+          <option value={0}>--Choose size--</option>
+          <option value={12.5}>--Large--</option>
+          <option value={8.5}>--Medium--</option>
+          <option value={5.6}>--Small--</option>
+        </select>
       </div>
     </div>
   );
